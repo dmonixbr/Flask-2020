7100524158b83cb425356ebbd63042e6001c699c
Victor Vieira de Melo <38594782+dmonixbr@users.noreply.github.com>
2020-06-06 02:56:06 -0300

    começo de relacionar prog a tech

--- sistema/programador/views.py
+++ sistema/programador/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from sistema import db
 from sistema.programador.models import Programador
+from sistema.tecnologia.models import Tecnologia
 
 programador = Blueprint('programador', __name__, template_folder="templates")
 
@@ -51,3 +52,19 @@ def excluir_programador(_id):
         return redirect(url_for('programador.index'))
 
     return render_template('excluir_programador.html', programador = programador)
+
+@programador.route('/atribuir_tecnologia/<int:_id>', methods=['POST','GET'])
+def atribuir_tecnologia(_id):
+    programador = Programador.query.get_or_404(_id)
+    tecnologias = Tecnologia.query.all()
+
+    if request.method == 'POST':
+        id_tecnologia = request.form['id_tecnologia']
+        tecnologia = Tecnologia.query.get_or_404(id_tecnologia)
+        programador.tecnologias.append(tecnologia)
+
+        db.session.commit()
+
+        return redirect(url_for('programador.index'))
+    return render_template('atrubuir_tecnologia.html', programador = programador, tecnologias = tecnologias)
+
