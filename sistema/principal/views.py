1ab45221df989927c66e2633052d3ec9d36cf252
Victor Vieira de Melo <38594782+dmonixbr@users.noreply.github.com>
2020-06-06 20:07:46 -0300

    Sistema Finalizado

--- sistema/principal/views.py
+++ sistema/principal/views.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template
+from sistema.programador.models import Programador
+from sistema.tecnologia.models import Tecnologia
 
 principal = Blueprint('principal', __name__)
 
 @principal.route('/')
 def index():
-    return render_template("home.html")
+    programadores   = Programador.query.all()
+    tecnologias     = Tecnologia.query.all()
+
+    return render_template("home.html", programadores=programadores, tecnologias=tecnologias)
